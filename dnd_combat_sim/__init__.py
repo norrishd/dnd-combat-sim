7aa24bcf4b6d60d03165820f82114e9dcfb451c9
David Norrish <norrish.d@gmail.com>
2024-03-13 12:37:02 +1100

    clean up combat logging

--- dnd_combat_sim/__init__.py
+++ dnd_combat_sim/__init__.py
@@ -1,5 +1,6 @@
 import logging
 
-logging.basicConfig(format="", level=logging.DEBUG)
+# logging.basicConfig(format="%(name)s %(message)s", level=logging.DEBUG)
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 logging.getLogger().setLevel(logging.DEBUG)
 logging.getLogger("asyncio").setLevel(logging.WARNING)
--- dnd_combat_sim/log.py
+++ dnd_combat_sim/log.py
@@ -5,6 +5,7 @@ from dnd_combat_sim.conditions import TempCondition
 
 from dnd_combat_sim.creature import Creature
 from dnd_combat_sim.rules import DamageOutcome
+from dnd_combat_sim.traits.weapon_traits import WeaponTrait
 from dnd_combat_sim.weapon import AttackDamage, AttackRoll, Weapon
 
 logger = logging.getLogger(__name__)
@@ -13,25 +14,20 @@ logger = logging.getLogger(__name__)
 class EncounterLogger:
     """Class to log details of an encounter."""
 
-    def __init__(self) -> None:
-        self.level = logging.DEBUG
-
     def log_encounter(self, i):
-        self._log_and_pause(f"\n*** Encounter {i + 1} ***", logging.INFO)
+        self._log_and_pause(f"\n\n*** Encounter {i + 1} ***", level=25)
 
     def log_roll_initiative(self, initiative: dict[Creature, int]):
         init_str: str = "\n".join(
             f"{creature}: rolled {initiative}" for creature, initiative in initiative.items()
         )
-        self._log_and_pause(f"Initiative:\n{init_str}", logging.INFO)
+        self._log_and_pause(f"Initiative:\n{init_str}", level=25)
 
     def log_start_round(self, round_number: int):
-        self._log_and_pause(f"* Round {round_number} *", level=logging.INFO)
+        self._log_and_pause(f"\n* Round {round_number} *")
 
     def log_choose_action(self, creature: Creature, action: str):
-        self._log_and_pause(
-            f"{creature.name} takes the {action} action. TODO implement", logging.INFO
-        )
+        self._log_and_pause(f"{creature.name} takes the {action} action. TODO implement")
 
     def log_attack_modifiers(
         self,
@@ -42,22 +38,23 @@ class EncounterLogger:
     ):
         msg = ""
         if weapon_modifiers:
-            msg += f"Weapon modifiers: {weapon_modifiers}\n"
+            msg += f"Weapon trait: {weapon_modifiers}\n"
         if attacker_trait_modifiers:
-            msg += f"Attacker trait modifiers: {attacker_trait_modifiers}\n"
+            msg += f"Attacker traits: {attacker_trait_modifiers}"
         if attacker_condition_modifiers:
-            msg += f"Attacker condition modifiers: {attacker_condition_modifiers}\n"
+            msg += f"Attacker conditions: {attacker_condition_modifiers}"
         if target_condition_modifiers:
-            msg += f"Target condition modifiers: {target_condition_modifiers}\n"
+            msg += f"Target conditions: {target_condition_modifiers}"
 
-        self._log_and_pause(msg, logging.INFO)
+        if msg:
+            self._log_and_pause(msg)
 
     def log_miss(self, attacker: Creature, target: Creature, attack_roll: AttackRoll):
         msg = (
             f"{attacker.name} attacks {target.name} with {attack_roll.weapon.name}: rolls "
-            f" {attack_roll}: miss"
+            f"{attack_roll}: misses"
         )
-        self._log_and_pause(msg, logging.INFO)
+        self._log_and_pause(msg)
 
     def log_hit(
         self,
@@ -66,31 +63,44 @@ class EncounterLogger:
         attack_roll: AttackRoll,
         attack_damage: AttackDamage,
         modified_damage: AttackDamage,
+        attack_traits_applied: list[WeaponTrait],
         damage_taken: AttackDamage,
+        damage_result: DamageOutcome,
     ):
-        msg = f"{attacker.name} attacks {target.name} with {attack_roll.weapon.name}: rolls {attack_roll}: "
-        msg += f"{'CRITS' if attack_roll.is_crit else 'hits'} for {modified_damage} damage"
+        if damage_result == DamageOutcome.still_dying:
+            msg = (
+                f"{attacker.name} attacks {target.name} while down with {attack_roll.weapon.name}: "
+            )
+            if attack_damage.crit:
+                msg += "CRITS for 2 automatic death saving throws"
+            else:
+                msg += "hits for an automatic death saving throw"
+            return self._log_and_pause(f"{target.name} is on death's door")
+
+        msg = (
+            f"{attacker.name} attacks {target.name} with {attack_roll.weapon.name}: "
+            f"rolls {attack_roll}: {'CRITS' if attack_roll.is_crit else 'hits'} for "
+            f"{modified_damage} damage"
+        )
         if attack_damage.total != damage_taken.total:
-            msg += f" (modified from {attack_damage})"
+            msg += f" (modified from {attack_damage} by {attack_traits_applied})"
+
+        self._log_and_pause(msg)
+        if damage_result == DamageOutcome.knocked_out:
+            self._log_and_pause(f"{target.name} is down!")
+        elif damage_result in {DamageOutcome.dead, DamageOutcome.instant_death}:
+            self._log_and_pause(f"{target.name} is DEAD!")
 
     def log_weapon_effects(self, weapon_effects: list[TempCondition]):
         if weapon_effects:
-            self._log_and_pause(f"New condition: {weapon_effects}", logging.INFO)
-
-    def log_attack_outcome(self, damage_result: DamageOutcome, target: Creature):
-        if damage_result == DamageOutcome.knocked_out:
-            self._log_and_pause(f"{target.name} is down!", level=logging.INFO)
-        elif damage_result == DamageOutcome.still_dying:
-            self._log_and_pause(f"{target.name} is on death's door", level=logging.INFO)
-        elif damage_result in {DamageOutcome.dead, DamageOutcome.instant_death}:
-            self._log_and_pause(f"{target.name} is DEAD!", level=logging.INFO)
+            self._log_and_pause(f"New condition: {weapon_effects}")
 
     def log_end_round(self, creatures: list[Creature]):
-        self._log_and_pause(creatures, logging.INFO)
+        self._log_and_pause(creatures)
 
     def log_winner(self, winner: Creature):
-        self._log_and_pause(f"Winner: {winner}")
+        self._log_and_pause(f"Winner: {winner}", level=25)
 
-    def _log_and_pause(self, message: str, level):
-        logger.log(self.level, message)
+    def _log_and_pause(self, message: str, level=logging.INFO):
+        logger.log(level, message)
         # time.sleep(sleep_time)
